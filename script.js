7df10d3cab0e80cb9cc3c1961f7b147d6a405035
Phoenix-0001 <149677560+Phoenix-0001@users.noreply.github.com>
2024-03-31 11:44:06 +0530

    Game not running after first collision

--- script.js
+++ script.js
@@ -37,6 +37,9 @@ function moveSnake() {
     score += 10;
     document.getElementById("score").innerText = "Score: " + score;
     generateFood();
+    setTimeout(() => {
+      drawFood();
+    }, 300);
   } else {
     snake.pop();
   }
@@ -50,56 +53,52 @@ function generateFood() {
 }
 
 function drawFood() {
-  ctx.beginPath();
-  ctx.arc(food.x,food.y,5,0,Math.PI*2);
-  ctx.fillStyle = "Red";
-  ctx.fill();
-
+  ctx.fillStyle = "red";
+  ctx.fillRect(food.x, food.y, 10, 10);
+}
+function gameOver() {
+  const head = snake[0];
+  return (
+    head.x < 0 ||
+    head.x >= 400 ||
+    head.y < 0 ||
+    head.y >= 400 ||
+    snake.slice(1).some((part) => part.x === head.x && part.y === head.y)
+  );
 }
 
-function gameOver(){
-    const head = snake[0];
-    return (
-        head.x <0 || head.x >=400 || head.y <0 || head.y>=400 ||
-        snake.slice(1).some(part => part.x ===head.x && part.y=== head.y)
-
-    );
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function clearCanvas(){
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+document.addEventListener("keydown", changeDirection);
+
+function changeDirection(event) {
+  const LEFT_KEY = 37;
+  const RIGHT_KEY = 39;
+  const UP_KEY = 38;
+  const DOWN_KEY = 40;
+  const keyPressed = event.keyCode;
+  const goingUp = dy === -10;
+  const goingDown = dy === 10;
+  const goingRight = dx === 10;
+  const goingLeft = dx === -10;
+
+  if (keyPressed === LEFT_KEY && !goingRight) {
+    dx = -10;
+    dy = 0;
+  }
+  if (keyPressed === UP_KEY && !goingDown) {
+    dx = 0;
+    dy = -10;
+  }
+  if (keyPressed === RIGHT_KEY && !goingLeft) {
+    dx = 10;
+    dy = 0;
+  }
+  if (keyPressed === DOWN_KEY && !goingUp) {
+    dx = 0;
+    dy = 10;
+  }
 }
-
-document.addEventListener('keydown',changeDirection);
-
-function changeDirection(event){
-    const LEFT_KEY =37;
-    const RIGHT_KEY =39;
-    const UP_KEY=38;
-    const DOWN_KEY=40;
-    const keyPressed= event.keyCode;
-    const goingUp=dy === -10;
-    const goingDown = dy === 10;
-    const goingRight = dx === 10;
-    const goingLeft = dx === -10;
-
-    if (keyPressed === LEFT_KEY && !goingRight){
-        dx=-10;
-        dy=0;
-    }
-    if (keyPressed === UP_KEY && !goingDown){
-        dx=0;
-        dy=-10;
-
-    }
-    if (keyPressed === RIGHT_KEY && !goingLeft){
-        dx=10;
-        dy=0;
-
-    }
-    if (keyPressed=== DOWN_KEY && !goingUp){
-        dx=0;
-        dy=10;
-    }
-}
-main()
+main();
